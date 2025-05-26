a4c7ca2b4757c775473c6404fede463e1ada6e6b
slavikovics <slavikovics@outlook.com>
2025-05-27 02:18:18 +0300

    Adjusting config file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
     API_URL: 'https://api.bsuirbot.site/Bsuir?modelId=0',
-    USE_RAG_API: false,
-    ANSWER_PIPELINE_API: ''
+    USE_RAG_API: true,
+    ANSWER_PIPELINE_API: 'http://localhost:8080/'
 };
