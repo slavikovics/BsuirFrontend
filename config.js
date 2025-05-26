29fea08ffa1773fa8e72cc9546be96f4a0b2c999
slavikovics <slavikovics@outlook.com>
2025-05-27 02:47:08 +0300

    Correct container URL

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const config = {
     API_URL: 'https://api.bsuirbot.site/Bsuir?modelId=0',
     USE_RAG_API: true,
     ANSWER_PIPELINE_API: 'http://localhost:8080/',
-    CORS_URL: 'http://localhost:8081'
+    CORS_URL: 'http://localhost:8081/'
 };
