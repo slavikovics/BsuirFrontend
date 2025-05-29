000eba4872b05c1820e43ccf99566936572d8dfc
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-05-29 17:48:27 +0300

    Editing entrypoint script to launch nginx before certbot

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 CERT_PATH="/etc/letsencrypt/live/bsuirbot.site/fullchain.pem"
 
+nginx -g "daemon off;" &
+
+NGINX_PID=$!
+
 if [ ! -f "$CERT_PATH" ]; then
   echo "SSL certificates not found, obtaining from Let's Encrypt..."
 
@@ -11,9 +15,11 @@ if [ ! -f "$CERT_PATH" ]; then
 
   if [ $? -ne 0 ]; then
     echo "Failed to obtain certificates"
+    kill $NGINX_PID
     exit 1
   fi
 fi
 
-echo "Certificates found, starting nginx..."
-exec nginx -g "daemon off;"
+kill $NGINX_PID
+
+nginx -g "daemon off;"
